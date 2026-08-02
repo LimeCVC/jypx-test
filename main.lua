d0210dabcb2076e66269920ba431e076c640619a
LimeCVC <kalinovskiymaksim2011@gmail.com>
2026-08-03 00:47:06 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -6072,7 +6072,7 @@ local function createUI()
 
     makeLabel("SHOP PURCHASES", miscFr)
     makeBtn("DragonH", "Dragon Harpoon", miscFr, function() workspace.PromptRobuxEvent:InvokeServer(1534841732,"Product") end)
-    makeBtn("CookieW", "Cookie Wheels", miscFr, function() workspace.PromptRobuxEvent:InvokeServer(1126385328,"Product") end)
+    makeBtn("CookieW", "Cookie Wheels", miscFr, function() workspace.PromptRobuxEvent:InvokeServer(1931849988,"Product") end)
     makeBtn("MegaT", "Orange Mega Turbines", miscFr, function() workspace.PromptRobuxEvent:InvokeServer(139121474,"Product") end)
     makeBtn("PineT", "Buy Pine Tree", miscFr, function() workspace.ItemBoughtFromShop:InvokeServer("PineTree",1) end)
 
